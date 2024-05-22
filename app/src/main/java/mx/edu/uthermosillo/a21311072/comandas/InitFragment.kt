24899a094d1f00893c87baab48ed5bcba172d87c
chuneee <xchune10@gmail.com>
2024-05-21 17:28:54 -0700

    initFragment se cambio utilizando viewBinding

--- app/src/main/java/mx/edu/uthermosillo/a21311072/comandas/InitFragment.kt
+++ app/src/main/java/mx/edu/uthermosillo/a21311072/comandas/InitFragment.kt
@@ -5,22 +5,25 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.viewpager2.widget.ViewPager2
 import mx.edu.uthermosillo.a21311072.comandas.adapters.OnBoardingAdapter
+import mx.edu.uthermosillo.a21311072.comandas.databinding.FragmentInitBinding
 import onboarding.FirstFragment
 import onboarding.SecondFragment
 import onboarding.ThirdFragment
 
-
 class InitFragment : Fragment() {
+
+    private var _binding: FragmentInitBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view: View = inflater.inflate(R.layout.fragment_init, container, false)
+        _binding = FragmentInitBinding.inflate(inflater, container, false)
+        val view = binding.root
 
-        val viewPager2 = view.findViewById<ViewPager2>(R.id.onBoardingViewPager)
+        val viewPager2 = binding.onBoardingViewPager
 
         val fragmentList = arrayListOf<Fragment>(
             FirstFragment(),
@@ -35,4 +38,8 @@ class InitFragment : Fragment() {
         return view
     }
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
